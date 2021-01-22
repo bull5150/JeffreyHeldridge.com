f568720a194bf498179352caae192d3d14d50220
Jeff Heldridge <bull5150@hotmail.com>
2021-01-22 09:18:20 -0600

    email string vs json fix

--- App/src/app/services/emailapi.service.ts
+++ App/src/app/services/emailapi.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { email } from 'src/app/models/email_models';
+import { email } from '../models/email_models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,6 @@ export class EmailapiService {
 
   public postEmail(email: email)
   {
-    return this.http.post<string>('api/email', email);
+    return this.http.post('api/email', email, {responseType: 'text'});
   }
 }
